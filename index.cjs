4c3ee21b40fffa9121d5675afafb013f5ccbc21b
Corey Farrell <git@cfware.com>
2020-03-08 15:46:45 -0400

    fix: eslint-comments/no-unused-disable warn instead of error

--- index.cjs
+++ index.cjs
@@ -73,7 +73,7 @@ const eslintCommentErrors = {
 	'eslint-comments/disable-enable-pair': [2, {allowWholeFile: true}],
 	'eslint-comments/no-aggregating-enable': 2,
 	'eslint-comments/no-duplicate-disable': 2,
-	'eslint-comments/no-unused-disable': 2,
+	'eslint-comments/no-unused-disable': 1,
 	'eslint-comments/no-unused-enable': 2
 };
 
